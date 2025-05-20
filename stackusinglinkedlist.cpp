5c423b0082827f5108bd0d8493ec88ffb33b2448
Kipril20 <rafky2004@gmail.com>
2025-05-20 11:05:55 +0700

    implementasi pemutaran linked list

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -58,3 +58,10 @@ public:
         {
             Node *current = top;
             while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }
+    }
